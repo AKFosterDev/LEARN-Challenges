5e8ed3e336910879bd2077289685271340105367
akfoster2015 <akfoster2015@gmail.com>
2020-11-11 13:01:55 -0800

    begin top5 challenge

---
+++ React/CreateNewReactApp/top5/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+import './App.css'
+
+class App extends Component {
+	render() {
+		return (
+			<Fragment>
+				<h1>Our Top 5</h1>
+			</Fragment>
+		)
+	}
+}
+
+export default App
