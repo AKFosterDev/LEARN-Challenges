2274ef1928e4c00f673d75715edac535e580c03b
AKFosterDev <akfoster2015@gmail.com>
2020-11-19 08:42:49 -0800

    psudo code

--- React/Tic-Tac-Toe/my-tic-tac-toe/src/components/Square.js
+++ React/Tic-Tac-Toe/my-tic-tac-toe/src/components/Square.js
@@ -1,22 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class Square extends Component {
-	
-//  create the handleClick 
-handleClick = () => {
-	this.props.handleGamePlay(this.props.index)
-}
+	//  create the handleClick
+	handleClick = () => {
+		this.props.handleGamePlay(this.props.index)
+	}
 
 	render() {
-		// const { value } = this.props;
+		const { value } = this.props
 
 		return (
 			<React.Fragment>
-				<div id='square' onClick={this.handleClick} >
-				
+				<div id='square' onClick={this.handleClick}>
+					{value}
 				</div>
 			</React.Fragment>
-		);
+		)
 	}
 }
-export default Square;
+export default Square
