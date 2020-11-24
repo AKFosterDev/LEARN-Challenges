235f19d82f438d2fba8c07455bd754ee7ef5bb2f
AKFosterDev <akfoster2015@gmail.com>
2020-11-23 17:56:31 -0800

    complete basic functionality between form and app.js

--- React/mad-libs/src/App.js
+++ React/mad-libs/src/App.js
@@ -3,11 +3,27 @@ import MadLibsForm from './components/MadLibForm'
 import './App.css'
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			formData: null,
+		}
+	}
+
+	handleFormSubmit = (form) => {
+		this.setState({ formData: form })
+	}
+
 	render() {
 		return (
 			<div className='App'>
 				<h1>Mad Libs App</h1>
-				<MadLibsForm />
+				<MadLibsForm handleFormSubmit={this.handleFormSubmit} />
+				{this.state.formData && (
+					<div>
+						<p>{this.state.formData.place}</p>
+					</div>
+				)}
 			</div>
 		)
 	}
--- React/mad-libs/src/components/MadLibForm.js
+++ React/mad-libs/src/components/MadLibForm.js
@@ -1,11 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 class MadLibForm extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			form: {
+				place: '',
+				number1: '',
+				number2: '',
+				adjective1: '',
+				noun1: '',
+				verb1: '',
+				adjective2: '',
+				adverb1: '',
+				adjective3: '',
+				verb2: '',
+				verb3: '',
+				adjective4: '',
+				noun2: '',
+				adjective5: '',
+				adjective6: '',
+				verb4: '',
+			},
+		}
+	}
+
+	handleChange = (e) => {
+		let { form } = this.state
+		form[e.target.name] = e.target.value
+		this.setState({ form: form })
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+		this.props.handleFormSubmit(this.state.form)
+	}
+
 	render() {
 		return (
-			<div>
+			<Fragment>
 				<h1>Mad Libs Form</h1>
-			</div>
+				<form>
+					<label>Proper noun/Place</label>
+					<input
+						type='text'
+						value={this.state.form.place}
+						name='place'
+						onChange={this.handleChange}
+					/>
+					<input type='submit' onClick={this.handleSubmit} value='Submit' />
+				</form>
+			</Fragment>
 		)
 	}
 }
